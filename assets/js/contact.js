1024ea7122285e3bc35bfd4fee25ecf35e0d9ef5
wimoMisterX <ran12us@gmail.com>
2016-11-05 21:45:33 +0800

    fixed contact us page

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,13 +3,13 @@ var m = require('mithril');
 var ContactUs = {
     controller: function(){
         var ctrl = this;
-
         ctrl.user = {
             name:  '',
+            email: '',
+            contact_num: '',
             message: ''
         };
         ctrl.err = '';
-
         ctrl.post = function(e){
             e.preventDefault();
             ctrl.success = 'Done !!';
@@ -17,23 +17,67 @@ var ContactUs = {
     },
     view: function(ctrl){
         return m('#Auth', [
-            m('h2', 'Contact Form'),
+            m('p.lead.text-center', 'Feel the need to contact us?'),
             m('form', {onsubmit: ctrl.post}, [
-                m('input[type="text"][placeholder="Name"]', {
-                    value: ctrl.user.name,
-                    onchange: function(e){
-                        ctrl.user.name = e.currentTarget.value;
-                    }
-                }),
-                m('input[type="text"][placeholder="Message"]', {
-                    value: ctrl.user.message,
-                    onchange: function(e){
-                        ctrl.user.message = e.currentTarget.value;
-                    }
-                }),
+                m('div.row',
+                    m('div.medium-6.medium-centered.columns',
+                        m('label', [
+                            'Name',
+                            m('input[type="text"][placeholder="Name"]', {
+                                value: ctrl.user.name,
+                                onchange: function(e){
+                                    ctrl.user.name = e.currentTarget.value;
+                                }
+                            })
+                        ])
+                    )
+                ),
+                m('div.row',
+                    m('div.medium-6.medium-centered.columns',
+                        m('label', [
+                            'Email',
+                            m('input[type="email"][placeholder="Email"]', {
+                                value: ctrl.user.email,
+                                onchange: function(e){
+                                    ctrl.user.email = e.currentTarget.value;
+                                }
+                            })
+                        ])
+                    )
+                ),
+                m('div.row',
+                    m('div.medium-6.medium-centered.columns',
+                        m('label', [
+                            'Contact Number',
+                            m('input[type="text"][placeholder="Contact Number"]', {
+                                value: ctrl.user.contact_num,
+                                onchange: function(e){
+                                    ctrl.user.contact_num = e.currentTarget.value;
+                                }
+                            })
+                        ])
+                    )
+                ),
+                m('div.row',
+                    m('div.medium-6.medium-centered.columns',
+                        m('label', [
+                            'Message',
+                            m('textarea[placeholder="Message"]', {
+                                value: ctrl.user.message,
+                                onchange: function(e){
+                                    ctrl.user.message = e.currentTarget.value;
+                                }
+                            })
+                        ])
+                    )
+                ),
                 (ctrl.err) ? m('p', ctrl.err) : '',
                 (ctrl.success) ? m('p', ctrl.success) : '',
-                m('button[type="submit"]', 'Log in'),
+                m('div.row',
+                    m('div.medium-6.medium-centered.text-center.columns',
+                        m('button.button[type="submit"]', 'Submit')
+                    )
+                ),
             ])
         ]);
     }
