ccc203f695c9141ce273c83bbbb732bf5215a39d
Wimodya Athukorala <ran12us@gmail.com>
2016-11-14 11:23:28 +0000

    added images loaded to flickty

--- src/app/home.js
+++ src/app/home.js
@@ -1,5 +1,5 @@
 var m = require('mithril');
-var Flickity = require('flickity');
+var Flickity = require('flickity-imagesloaded');
 
 var SAMPLE_PARAGRAPH = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
@@ -19,6 +19,8 @@ var Home = {
                 adaptiveHeight: true,
                 wrapAround: true,
                 autoPlay: true,
+		cellAlign: 'left',
+		imagesLoaded: true,
             });
         }.bind(this)
     },
