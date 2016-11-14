4fe39f68599ac01df3011dab32b40b0dc3e3abd9
wimoMisterX <ran12us@gmail.com>
2016-11-14 21:42:00 +0800

    addded image prefetch

--- src/app/main.js
+++ src/app/main.js
@@ -1,17 +1,24 @@
 var m = require('mithril');
+var image_preloader = require('image-preloader');
+var preloader = new image_preloader;
+
 var contact = require('./contact.js');
 var services = require('./services.js');
 var menu = require('./menu.js');
 var about = require('./about.js');
 var home = require('./home.js');
 
-m.route.mode  = "pathname";
-m.route(document.getElementById("content"), "/", {
-    "/": home.Home,
-    "/services": services.Services,
-    "/contact": contact.ContactUs,
-    "/about": about.AboutUs,
-});
+var image_set = ['images/hotel-1.jpg', 'images/hotel-2.jpg', 'images/sample.jpg', 'images/service-station.jpg', 'images/service-station-2.jpg', 'images/shopping-1.jpg', 'images/shopping-2.jpg'];
 
-m.mount(document.getElementById("header-menu-items"), menu.header_menu);
-m.mount(document.getElementById("footer-menu-items"), menu.footer_menu);
+preloader.preload.apply(this, image_set).then(function(status){
+    m.route.mode  = "pathname";
+    m.route(document.getElementById("content"), "/", {
+        "/": home.Home,
+        "/services": services.Services,
+        "/contact": contact.ContactUs,
+        "/about": about.AboutUs,
+    });
+
+    m.mount(document.getElementById("header-menu-items"), menu.header_menu);
+    m.mount(document.getElementById("footer-menu-items"), menu.footer_menu);
+});
