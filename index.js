0875f5d415ba12a235ceaa71b5a55a9ff6158e69
wimoMisterX <ran12us@gmail.com>
2017-07-17 17:06:50 +0800

    restructured config files

--- index.js
+++ index.js
@@ -9,27 +9,13 @@ var fs = require('fs');
 var config = require('config');
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('short'));
+app.use(express.static(__dirname + '/src/assets'));
+app.use(favicon(__dirname + '/src/assets/images/favicon.ico'));
 
-if (config.has('Mail.email') && config.has('Mail.password')){
-    var auth_settings = {
-        user: config.Mail.email,
-        pass: config.Mail.password
-    };
-}else{
-    var auth_settings = {
-        type: 'OAuth2',
-        user: config.Mail.user,
-        clientId: config.Mail.clientId,
-        clientSecret: config.Mail.clientSecret,
-        refreshToken: config.Mail.refreshToken,
-    };
-}
-
-var smtpTrans = nodemailer.createTransport({
-    service: config.Mail.service,
-    auth: auth_settings,
-});
-
+var smtpTrans = nodemailer.createTransport(config.mailer);
 smtpTrans.verify(function(error, succ){
     if (error) {
         console.log(error);
@@ -38,32 +24,25 @@ smtpTrans.verify(function(error, succ){
     }
 })
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(morgan('dev'));
-app.use(express.static(__dirname + '/src/assets'));
-app.use(favicon(__dirname + '/src/assets/images/favicon.ico'));
-
 app.get('*', (req, res) => {
     var spa_file = fs.readFileSync(__dirname + '/src/index.html', 'utf8');
     var spa_template = hogan.compile(spa_file);
     res.send(spa_template.render({
         google_maps_api_key: config.google_maps_api_key,
-        recaptcha_client_key: config.Recaptcha.client_key,
         current_year: new Date().getFullYear(),
     }));
 });
 
 app.post('/contact/send-mail', (req, res) => {
     var form_data = req.body;
-    request.post('https://www.google.com/recaptcha/api/siteverify', {form: {secret: config.Recaptcha.server_key, response: req.body.captcha}}, function(err, httpResponse, body){
+    request.post('https://www.google.com/recaptcha/api/siteverify', {form: {secret: config.recaptcha_server_key, response: req.body.captcha}}, function(err, httpResponse, body){
         body = JSON.parse(body);
         if (err || !body.success){
             res.json({type: 'alert', message: 'Error occured, invalid captcha'});
         }else{
             var company_mail = {
-                from: 'Wasantha Athukorala Website ' + config.Mail.support_email,
-                to: config.Mail.our_email,
+                from: 'Wasantha Athukorala Website ' + config.support_email,
+                to: config.our_email,
                 subject: 'You have got a new message!',
                 text: 'The following message was sent by ' + form_data.name + '\n' +
                       'Contact number is ' + (form_data.contact_num || '-') + '\n' +
@@ -80,7 +59,7 @@ app.post('/contact/send-mail', (req, res) => {
             var email_file = fs.readFileSync(__dirname + '/src/thank_you_email.html', 'utf8');
             var email_template = hogan.compile(email_file);
             var client_mail = {
-                from: 'Wasantha Athukorala Sole Propreitorship ' + config.Mail.support_email,
+                from: 'Wasantha Athukorala Sole Propreitorship ' + config.support_email,
                 to: form_data.email,
                 subject: 'Enquriy at Wasantha Athukorala Sole Propreitorship',
                 html: email_template.render({name: form_data.name})
